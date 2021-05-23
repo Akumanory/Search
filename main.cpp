51a30cca61b7a06e95004bffe50b412874e92e42
Akumanory <kencher87@gmail.com>
2021-05-23 23:38:00 +0300

    Done

--- main.cpp
+++ main.cpp
@@ -6,4 +6,18 @@ int main()
     Dictionary<int, int> a;
     a.put(1, 10);
     a.put(3, 20);
+    a.put(4, 30);
+
+    a.show();
+
+    a.iterator();
+
+    a.test_min_max();
+
+    std::cout << "Key: " << a[23] << std::endl;
+
+    for (auto it = a.iterator(); it.hasNext(); it.next())
+    {
+        std::cout << "Key: " << it.key() << std::endl;
+    }
 }
